cfcb1ba0a30cc2b759fdeed0a5afe80e2472ecbf
deaf <ts-2001@bk.ru>
2026-01-23 03:09:17 +0500

    fix: сервер слушает 0.0.0.0 для работы с Docker

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ import fastify from "./app.js"
 
 
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ 
+    port: 3000,
+    host: "0.0.0.0"
+  })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
